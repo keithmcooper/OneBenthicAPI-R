d635108ee9cf6ffda4af2fc1df71e4bdd3c61e2f
Keith M Cooper <52048205+keithmcooper@users.noreply.github.com>
2022-09-16 18:52:02 +0100

    add api 5. nNote spaces in survey name are encoded with %20

--- OneBenthicAPI-R.R
+++ OneBenthicAPI-R.R
@@ -117,8 +117,36 @@ str(data_text)
 View(data_text)
 
 #OR
-
+# Use this code if you have multiple values. Here, values are added to the url for year = 2000 and 2001.
 url_base <- "https://rconnectdev.corp.cefas.co.uk/content/148/S?in_year=2013&in_year=2001"
+data <- GET(url_base)
+
+## Retrieve contents of a request and make into a df
+data_text <- content(data, "text")%>%fromJSON
+
+## Explore returned data
+head(data_text)
+#_______________________________________________________________________________
+#### OneBenthicAPI-5 ####
+
+##EITHER use this code if you are returning data where you have only a single value in the search parameter 'year'.
+## Url of the api
+url_base <- "https://rconnectdev.corp.cefas.co.uk/content/149/S?"
+
+## Retrieve information from the request URL (inc query parameters). Enter parameter values in list below. Separate items using '&'
+data <- GET(url_base,query=list(in_Survey_name = 'Eastern English Channel Regional Seabed Monitoring Programme 2020'))
+
+## Retrieve contents of a request and make into a df
+data_text <- content(data, "text")%>%fromJSON
+
+## Explore returned data
+class(data_text)
+str(data_text)
+View(data_text)
+
+#OR
+# Use this code if you have multiple values. Here, survey names are added to the url. Spaces in survey name must be encoded as '%20'.
+url_base <- "https://rconnectdev.corp.cefas.co.uk/content/149/S?in_Survey_name=Eastern%20English%20Channel%20Regional%20Seabed%20Monitoring%20Programme%202020"
 data <- GET(url_base)
 
 ## Retrieve contents of a request and make into a df
