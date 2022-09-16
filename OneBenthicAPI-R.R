7272750812da07b4c14111b082d8d6d892738448
Keith M Cooper <52048205+keithmcooper@users.noreply.github.com>
2022-09-16 19:07:45 +0100

    add api6

--- OneBenthicAPI-R.R
+++ OneBenthicAPI-R.R
@@ -155,3 +155,31 @@ data_text <- content(data, "text")%>%fromJSON
 ## Explore returned data
 head(data_text)
 #_______________________________________________________________________________
+#### OneBenthicAPI-6 ####
+
+##EITHER use this code if you are returning data where you have only a single value in the search parameter 'year'.
+## Url of the api
+url_base <- "https://rconnectdev.corp.cefas.co.uk/content/150/S?"
+
+## Retrieve information from the request URL (inc query parameters). Enter parameter values in list below. Separate items using '&'
+data <- GET(url_base,query=list(in_surveyname = 'Eastern English Channel Regional Seabed Monitoring Programme 2020'))
+
+## Retrieve contents of a request and make into a df
+data_text <- content(data, "text")%>%fromJSON
+
+## Explore returned data
+class(data_text)
+str(data_text)
+View(data_text)
+
+#OR
+# Use this code if you have multiple values. Here, survey names are added to the url. Spaces in survey name must be encoded as '%20'.
+url_base <- "https://rconnectdev.corp.cefas.co.uk/content/150/S?in_surveyname=Eastern%20English%20Channel%20Regional%20Seabed%20Monitoring%20Programme%202020"
+data <- GET(url_base)
+
+## Retrieve contents of a request and make into a df
+data_text <- content(data, "text")%>%fromJSON
+
+## Explore returned data
+head(data_text)
+#_______________________________________________________________________________
