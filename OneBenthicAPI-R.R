140c0aaad9de6e9107d41d99af6ddcb14d5cf320
Keith M Cooper <52048205+keithmcooper@users.noreply.github.com>
2022-09-16 10:29:24 +0100

    minor

--- OneBenthicAPI-R.R
+++ OneBenthicAPI-R.R
@@ -18,8 +18,8 @@ library(dplyr)
 url_base <- "https://rconnectdev.corp.cefas.co.uk/content/144/S?"
 
 ## Use this code to retrieve information from the request URL (inc query parameters) for parameters 'year' and 'sieve_size_mm' with only a single value for each
-data <- GET(url_base,query=list(in_year = '2000',
-                                in_sieve_size_mm = '1'))
+data <- GET(url_base,query=list(in_year = 2000,
+                                in_sieve_size_mm = 1))
 
 ## Retrieve contents of a request and make into a df
 data_text <- content(data, "text")%>%fromJSON
